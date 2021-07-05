8c230e02d6c965ae73fa086b9b365e2c49764141
jonghyeon <fish560114@gmail.com>
2021-07-05 17:52:22 +0900

    브론즈4 A/B 풀이중

---
+++ bronze/lv4/devide.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+int main(int argc, char* argv[]){
+
+
+	double N,M;
+	scanf("%f",&N);
+	scanf("%f",&M);
+	
+	printf("%.33f",N/M);
+	
+	
+	return 0;
+}
